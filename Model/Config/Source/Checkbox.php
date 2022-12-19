4bfc60a6940df44e8ba711c9040544aa2cdf3b03
nikunjv9737 <nikunjv9737@gmail.com>
2022-12-19 12:12:21 +0530

    add sku limited code on config file

---
+++ Model/Config/Source/Checkbox.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace DamConsultants\Bynder\Model\Config\Source;
+/**
+ * Used in creating options for getting product type value
+ *
+ */
+class Checkbox
+{
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return [['value' => 'checkbox', 'label'=>__('Get All Sku')]];
+    }
+}
